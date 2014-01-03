9a1661b9594d84a3564e4a25df57e03819edad83
teopii <teopii@gmail.com>
2014-01-03 16:55:59 +0200

    tuntikirjanpito

--- Sokrates/src/main/java/sokrates/kayttoliittyma/App.java
+++ Sokrates/src/main/java/sokrates/kayttoliittyma/App.java
@@ -1,7 +1,17 @@
 package sokrates.kayttoliittyma;
 
+/**
+ * Luokka App tapahtuu ohjelma käynnistettäessä
+ *
+ * @author Teo
+ */
 public class App {
 
+    /**
+     * Metodi luo ja suorituttaa() Sovelluksen
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         new Sovellus().suorita();
     }
